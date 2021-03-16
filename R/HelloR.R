ada73ede5b1c1299990d5308eefec70903296cc8
Max Liu <renjing.liu@mail.utoronto.ca>
2021-03-16 19:54:02 +0800

    Function for package

--- R/HelloR.R
+++ R/HelloR.R
@@ -1,2 +1,15 @@
 
+library(MaxliuRpackage)
+input_1 <- 20
+mySum <- function(input_1, input_2 = 10) {
+  output <- input_1 + input_2
+  return(output)
+}
+
+
+center <- function(data, midpoint) {
+  new_data <- (data - mean(data)) + midpoint
+  return(new_data)
+}
+
 
